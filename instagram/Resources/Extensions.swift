334220e6480c6da228bc7decd3d1af3f50950d45
eunseock <dortus47@naver.com>
2021-10-28 14:51:55 +0900

    - 로그인 뷰 구현
    - 아이디, 비밀번호 입력 필드
    - 로그인 버튼
    - 이용약관 정책 이동

---
+++ instagram/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  instagram
+//
+//  Created by 장은석 on 2021/10/28.
+//
+
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
