f5a9053c96a80b5c9677d33ba9aac8f8b178bd68
eunseock <dortus47@naver.com>
2021-11-04 22:43:07 +0900

    - 앱 로고, 포토 관련 컬렉션 뷰 추가

---
+++ instagram/Models/Models.swift
@@ -0,0 +1,63 @@
+//
+//  Models.swift
+//  instagram
+//
+//  Created by 장은석 on 2021/11/04.
+//
+
+import Foundation
+
+enum Gender {
+    case male, female, other
+}
+
+struct USer {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthDate: Date
+    let gender: Gender
+    let counts: USerCount
+    let joinDate: Date
+}
+
+struct USerCount {
+    let followers: Int
+    let following: Int
+    let posts: Int
+}
+
+public enum UserPostType {
+    case photo, video
+}
+
+/// Represents a user Post
+public struct UserPost {
+    let identifier: String
+    let postType: UserPostType
+    let thumbnailImage: URL
+    let postURL: URL // either video url or full resolution photo
+    let caption: String?
+    let likeCount: [PostLikes]
+    let comments: [PostComment]
+    let createdDate: Date
+    let taggedUsers: [String]
+}
+
+struct PostLikes {
+    let username: String
+    let PostIdentifier: String
+}
+
+struct CommentLike {
+    let username: String
+    let commentIdentifier: String
+}
+
+struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
+}
